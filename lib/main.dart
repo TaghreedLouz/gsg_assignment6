c18bae08e23c7d85a2808ea3fe84fc0afec7b9f3
Taghreed Louz <121046503+TaghreedLouz@users.noreply.github.com>
2025-08-24 14:23:53 +0300

    signup, login, Navigator,form,TextEditingController, routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:gsg_assignment6/routes.dart';
 import 'package:gsg_assignment6/screens/home_screen.dart';
+import 'package:gsg_assignment6/screens/login_screen.dart';
+import 'package:gsg_assignment6/screens/signup_screen.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: HomeScreen(),
+    home: LoginScreen(),
+    routes: {
+      Routes.login: (context) => LoginScreen(),
+      Routes.home: (context) => HomeScreen(),
+      Routes.signup: (context) => SignupScreen(),
+    },
   ));
 }
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:gsg_assignment6/routes.dart';
+import 'package:gsg_assignment6/widgets/custom_text_field_widget.dart';
+
+class LoginScreen extends StatelessWidget {
+  LoginScreen({super.key});
+
+  final _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(centerTitle: true,title: Text("Login",style: TextStyle(color: Colors.black)),backgroundColor: Colors.white),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 6,horizontal: 30),
+        child: Center(child:Form(
+          key: _formKey,
+            child: Column(children: [
+              Image.network("https://www.shutterstock.com/image-vector/computer-account-login-password-form-600nw-2316101921.jpg",height: 300,width: 400,),
+
+              CustomTextFieldWidget(controller: emailController,hint: "Email"
+                ,validate: (email) {
+                if(email!.contains("@") && email.contains(".")){
+                  return null;
+                }
+                return "Enter valid email";
+              },),
+
+              CustomTextFieldWidget(controller: passwordController,hint: "Password",isPassword: true
+                ,validate: (password) {
+                  if (password!.length >= 8) return null;
+                  return "Weak password";
+                },),
+
+              TextButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, Routes.signup);
+                },
+                child: Text("Didnt have an account?"),
+              ),
+              ElevatedButton(
+                onPressed: () => _login(context),
+                child: Text("Login"),
+              ),
+
+
+        ],))),
+      ),
+    );
+  }
+  _login(BuildContext context) {
+    if (_formKey.currentState!.validate()) {
+      Navigator.pushReplacementNamed(context, Routes.home);
+
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Enter valid credentials"),
+          duration: Duration(milliseconds: 500),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  }
+}
